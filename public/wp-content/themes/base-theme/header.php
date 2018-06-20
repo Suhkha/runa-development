d513b0652f7435dafaa20bc1303f7295d6f71c9d
Jessica Erandi Villa Bárcenas <suhkha92@gmail.com>
2018-06-20 15:13:10 -0500

    Basic header

--- public/wp-content/themes/base-theme/header.php
+++ public/wp-content/themes/base-theme/header.php
@@ -1,1 +1,14 @@
-<?php wp_nav_menu(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Development</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap core CSS -->
+  <link href="<?php bloginfo('template_directory'); ?>/assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/styles/main.css" rel="stylesheet">
+</head>
+
+<body>
+	<?php get_template_part('includes/menu', 'menu'); ?>
+</body>
---
+++ public/wp-content/themes/base-theme/includes/menu.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light navbar--yellow">
+  <a class="navbar-brand" href="#">Development</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link nav--white" href="#">Nosotros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link nav--white" href="#">Blog</a>
+      </li>
+    </ul>
+    <?php wp_nav_menu(); ?>
+  </div>
+</nav>
