4b9c0cc3e4f36c73bd5044f3f22537129bc5e8a4
Jessica Erandi Villa Bárcenas <suhkha92@gmail.com>
2018-06-20 16:11:20 -0500

    Stylizing the menu

--- public/wp-content/themes/base-theme/header.php
+++ public/wp-content/themes/base-theme/header.php
@@ -6,9 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Bootstrap core CSS -->
   <link href="<?php bloginfo('template_directory'); ?>/assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/styles/main.css" rel="stylesheet">
 </head>
 
 <body>
-	<?php get_template_part('includes/menu', 'menu'); ?>
+	<main>
+		<?php get_template_part('includes/menu', 'menu'); ?>
+	</main>
 </body>
